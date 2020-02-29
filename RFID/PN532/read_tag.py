b1a1b9e4f02fcb78cc05e5f1a3bb06cbb4fb6747
Cirignaco Enrico <e.cirignaco@gmail.com>
2020-02-29 12:07:40 +0100

    ID of Tag and Galaxy S8 are now storedin script and an if-else statement control if the readed ID math with one of the known IDs. The Program return 0 or 1 and the readed ID.

--- RFID/PN532/read_tag.py
+++ RFID/PN532/read_tag.py
@@ -6,8 +6,25 @@ from py532lib.i2c import *
 from py532lib.frame import *
 from py532lib.constants import *
 
+# create object for RFID mudule
 pn532 = Pn532_i2c()
 pn532.SAMconfigure()
 
-card_data = pn532.read_mifare().get_data()
-print(bytes(card_data).hex())
+# store known IDs
+tag_id = '4b01010004080439a91ba3'
+galaxys8_id = '533272'
+
+# read near device'ID
+readed_id = pn532.read_mifare().get_data()
+# convert bytearray to hey string
+readed_id = bytes(readed_id).hex()
+
+# check if readed ID match one of the known IDs
+if readed_id == galaxys8_id:
+	print('1')
+elif readed_id == tag_id:
+	print ('1')
+else:
+	print('0')
+
+print(readed_id)
