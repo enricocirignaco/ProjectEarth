cc0e5baed9c0fd73b4b21bdda663ffc3471cad99
Cirignaco Enrico <e.cirignaco@gmail.com>
2020-02-29 13:07:34 +0100

    try-except statement added to catch arrors.

--- RFID/PN532/read_tag.py
+++ RFID/PN532/read_tag.py
@@ -1,31 +1,34 @@
-import sys
 
-sys.path.insert(0, "/home/pi/Documents/git/ProjectEarth/libs/python/py532lib")
+try:
+	import sys
 
-from py532lib.i2c import *
-from py532lib.frame import *
-from py532lib.constants import *
+	sys.path.insert(0, "/home/pi/Documents/git/ProjectEarth/libs/python/py532lib")
 
-# create object for RFID mudule
-pn532 = Pn532_i2c()
-pn532.SAMconfigure()
+	from py532lib.i2c import *
+	from py532lib.frame import *
+	from py532lib.constants import *
 
-# store known IDs
-tag_id = '4b01010004080439a91ba3'
-galaxys8_id_a = '4b01010004200408'
-galaxys8_id_b = '0578807802'
+	# create object for RFID mudule
+	pn532 = Pn532_i2c()
+	pn532.SAMconfigure()
 
-# read near device'ID
-readed_id = pn532.read_mifare().get_data()
-# convert bytearray to hey string
-readed_id = bytes(readed_id).hex()
+	# store known IDs
+	tag_id = '4b01010004080439a91ba3'
+	galaxys8_id_a = '4b01010004200408'
+	galaxys8_id_b = '0578807802'
 
-# check if readed ID match one of the known IDs
-if readed_id == tag_id:
-	print('1')
-elif readed_id[0:16] == galaxys8_id_a:
-	if readed_id[22:32] == galaxys8_id_b:
+	# read near device'ID
+	readed_id = pn532.read_mifare().get_data()
+	# convert bytearray to hey string
+	readed_id = bytes(readed_id).hex()
+
+	# check if readed ID match one of the known IDs
+	if readed_id == tag_id:
 		print('1')
-else:
-	print('0')
-
+	elif readed_id[0:16] == galaxys8_id_a:
+		if readed_id[22:32] == galaxys8_id_b:
+			print('1')
+	else:
+		print('0')
+except:
+	print("error")
