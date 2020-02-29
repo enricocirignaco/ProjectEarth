91356ecc2fd18438965d4e3456d4e3c57a0462f4
Cirignaco Enrico <e.cirignaco@gmail.com>
2020-02-29 12:55:31 +0100

    Bug fixed --> Readed value from galaxy S8 is not always the same. a part of it is generated(16-32). The generated part was isolated and cut off from the if-else statment. Now both tag and phone return always 1 and others 0.

--- RFID/PN532/read_tag.py
+++ RFID/PN532/read_tag.py
@@ -12,7 +12,8 @@ pn532.SAMconfigure()
 
 # store known IDs
 tag_id = '4b01010004080439a91ba3'
-galaxys8_id = '533272'
+galaxys8_id_a = '4b01010004200408'
+galaxys8_id_b = '0578807802'
 
 # read near device'ID
 readed_id = pn532.read_mifare().get_data()
@@ -20,11 +21,11 @@ readed_id = pn532.read_mifare().get_data()
 readed_id = bytes(readed_id).hex()
 
 # check if readed ID match one of the known IDs
-if readed_id == galaxys8_id:
+if readed_id == tag_id:
 	print('1')
-elif readed_id == tag_id:
-	print ('1')
+elif readed_id[0:16] == galaxys8_id_a:
+	if readed_id[22:32] == galaxys8_id_b:
+		print('1')
 else:
 	print('0')
 
-print(readed_id)
