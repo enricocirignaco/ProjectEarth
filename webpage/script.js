8259198edb30347ce45c90a513f3f266f60fc511
Cirignaco Enrico <e.cirignaco@gmail.com>
2020-03-05 11:07:22 +0100

    Comments and minor changes added to webpages files. Branch now ready to merge

--- webpage/script.js
+++ webpage/script.js
@@ -1,20 +1,27 @@
+// Script to get current date and  time and display it on all the HTML pages
+
 function startTime() {
+  // get values
   var today = new Date();
+ 
   var y = today.getFullYear();
-  var mt = today.getMonth();
+  var mt= today.getMonth();
   var d = today.getDate();
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
   var w = today.getDay();
   
-  mt = checkTime(mt);
+  // add zero in front of numbers < 10
+  mt= checkTime(mt);
   d = checkTime(d);
   h = checkTime(h);
   m = checkTime(m);
   s = checkTime(s);
+  // convert 0-6 to weekdays
   w = checkDay(w);
   
+  // Display Values
   document.getElementById('weekDay').innerHTML = w;
   document.getElementById('date').innerHTML = d + ":" + mt + ":" + y;
   document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
@@ -25,9 +32,10 @@ function checkTime(i) {
   if (i < 10) 
   { 
     i = "0" + i 
-  } // add zero in front of numbers < 10
+  }
   return i;
 } 
+
 function checkDay(i) 
 {
   switch (i) {
