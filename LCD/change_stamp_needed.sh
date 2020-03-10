ca4059e137c2e4a5ab2bf1e06dfbad5aa27cad72
Cirignaco Enrico <e.cirignaco@gmail.com>
2020-03-10 21:49:13 +0100

    Bugfix: Wrong syntax xdotool in change_stamp_needed.sh.

--- LCD/change_stamp_needed.sh
+++ LCD/change_stamp_needed.sh
@@ -5,11 +5,11 @@ CHECKIN_LUNCH="12:10"
 CHECKOUT_EVENING="16:40"
 
 while [ "$(date +%H:%M)" != $CHECKOUT_LUNCH ]; do sleep 1; done
-#xdotool ctrl+key+3 # Display red tab
-echo bye
+xdotool key ctrl+3 # Display red tab
+
 while [ "$(date +%H:%M)" != $CHECKIN_LUNCH ]; do sleep 1; done
-xdotool ctrl+key+3 # Display red tab
+xdotool key ctrl+3 # Display red tab
 
 while [ "$(date +%H:%M)" != $CHECKOUT_EVENING ]; do sleep 1; done
-xdotool ctrl+key+3 # Display red tab
+xdotool key ctrl+3 # Display red tab
 
