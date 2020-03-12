b31959a62e26fb74d64c54f824afea47e48b32db
Cirignaco Enrico <e.cirignaco@gmail.com>
2020-03-12 07:44:06 +0100

    start.sh script run now on every bootup(is run by the autostart script. The autostart script is automaticaly modified by running the setup script.

--- setup/set_server_x.sh
+++ setup/set_server_x.sh
@@ -2,3 +2,6 @@
 
 #replace original lightdm.conf file with modified one (display timeout + mouse cursor)
 sudo cp lightdm.conf /etc/lightdm/lightdm.conf
+
+#copy autostart file to LXDE config directory (autostart)
+sudo cp autostart /etc/xdg/lxsession/LXDE-pi/autostart
