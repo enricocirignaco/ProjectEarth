de0a3bb4ac41e8df1340315ea6242d9d1895ab87
Cirignaco Enrico <e.cirignaco@gmail.com>
2020-03-12 07:41:14 +0100

    cd added before to run script. start.sh now can be run from any other location (dependecies will not break)

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # launch script to change tab on given time
+cd /home/pi/Documents/git/ProjectEarth
 ./LCD/change_stamp_needed.sh &
 
 # launch python script to read RFID
 cd ./RFID/PN532
 python3 ./read_tag.py &
-cd ../../LCD
+
 #open chromium tabs an login
+cd ../../LCD
 ./open_browser.sh &
