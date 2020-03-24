fb9073a2cd54bba5a6d0469046177a0ba9b16802
Cirignaco Enrico <e.cirignaco@gmail.com>
2020-03-24 11:46:40 +0100

    rotate display, enable spi and i2c added to configuration.

--- setup/setup_lcd.sh
+++ setup/setup_lcd.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-#install LCD driver and rotate display
+
+
+# replace original lightdm.conf file with modified one.
+# mouse cursor hided
+# lcd-off timeout disabled
+cp ./lightdm.conf /etc/lightdm/lightdm.conf
+
+# copy autostart file to LXDE config directory.
+# run the application automatically on startup.
+cp ./autostart /etc/xdg/lxsession/LXDE-pi/autostart
+
+# raplace original config.txt file with modified one.
+#enable spi
+#enable i2c
+#change display orientation (+90)
+cp ./config.txt /boot/config.txt
+
+
+#must be the last command of the script (afterwards system will reboot.)
+#install lcd drivers
 cd ./LCD-show
 chmod +x LCD35-show
 ./LCD35-show
-./rotate.sh 270
