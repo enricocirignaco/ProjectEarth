52397a2ab832ccc01f821da079180d0cbbc50487
enrico cirignaco <e.cirignaco@gmail.com>
2020-03-29 22:34:23 +0200

    echoing information after installation and finally reboot machine.

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ sudo apt-get update
 sudo apt-get upgrade
 git submodule update --init --recursive
 sudo ./setup_lcd.sh
+echo "installation completed. Raspberry will reboot now"
+sleep 1
+sudo reboot
