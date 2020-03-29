b4ec2b981ed1b7a38fd8c313c74f5c16d6fd3948
enrico cirignaco <e.cirignaco@gmail.com>
2020-03-29 22:19:31 +0200

    Own config.txt file removed. i2c and spi are already enabled in the config.txt file used by LCD-show.

--- setup/setup_lcd.sh
+++ setup/setup_lcd.sh
@@ -10,18 +10,10 @@ cp ./lightdm.conf /etc/lightdm/lightdm.conf
 # run the application automatically on startup.
 cp ./autostart /etc/xdg/lxsession/LXDE-pi/autostart
 
-# raplace original config.txt file with modified one.
-#enable spi
-#enable i2c
-#change display orientation (+90)
-cp ./config.txt /boot/config.txt
-
-
-#must be the last command of the script (afterwards system will reboot.)
-#install lcd drivers
+#install lcd drivers (Warning /boot/config.txt will be replaced!)
 cd ./LCD-show
 # update submodule to latest master-commit
 git pull origin master
-chmod +x LCD35-show
+chmod +x LCD35-show_no_reboot
 #rotate display to 270° without reboot
 ./LCD35-show_no_reboot 270
