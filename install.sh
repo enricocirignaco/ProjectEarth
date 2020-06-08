e2532ae77b25114e24d1eebcb4d0106c5044119d
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-06-08 21:07:46 +0200

    -y option added to apt command

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd ./setup
 sudo apt update
 sudo apt upgrade
 git submodule update --init --recursive
-sudo apt install xdotool
+sudo apt install xdotool -y
 sudo ./setup_lcd.sh
 echo "installation completed. Raspberry will reboot now"
 sleep 1
