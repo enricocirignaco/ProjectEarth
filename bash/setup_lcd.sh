067213a778d384067f08d95a647c6d9f779297ef
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-06-15 16:23:43 +0200

    Refactoring: Local paths changed after refactoring.

--- bash/setup_lcd.sh
+++ bash/setup_lcd.sh
@@ -4,17 +4,18 @@
 # replace original lightdm.conf file with modified one.
 # mouse cursor hided
 # lcd-off timeout disabled
-cp ./lightdm.conf /etc/lightdm/lightdm.conf
+cp ./ressources/lightdm.conf /etc/lightdm/lightdm.conf
 
 # copy autostart file to LXDE config directory.
 # run the application automatically on startup.
 # path of the start.sh script must updated in autostart file if changed.
-cp ./autostart /etc/xdg/lxsession/LXDE-pi/autostart
+cp ./ressources/autostart /etc/xdg/lxsession/LXDE-pi/autostart
 
 #install lcd drivers (Warning /boot/config.txt will be replaced!)
-cd ./LCD-show
+cd ./ressources/LCD-show
 # update submodule to latest master-commit
 git pull origin master
 chmod +x LCD35-show_no_reboot
 #rotate display to 270° without reboot
 ./LCD35-show_no_reboot 270
+cd ../../
