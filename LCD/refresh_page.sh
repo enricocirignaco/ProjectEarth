a160de8ad5c8268925195db94560967b260bf5f1
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-06-15 15:07:19 +0200

    Simulate alt+shift+r in order to reload all open tabs every 5 minutes.

--- LCD/refresh_page.sh
+++ LCD/refresh_page.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sleep 300
-xdotool key ctrl+r
+#Chrome Extension -Reload All Tabs- needed in order for script to work (https://chrome.google.com/webstore/detail/reload-all-tabs/midkcinmplflbiflboepnahkboeonkam?hl=en)
+#endless loop
+while true
+do
+	#sleep for 5 minutes
+	sleep 300
+	#refresh page
+	xdotool key alt+shift+r
+done
