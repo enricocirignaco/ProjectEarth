81d8a0b456144b20a188c0f79b5d3f3b824b3424
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-08-11 11:41:18 +0200

    Add script to programmatically add cron job at setup (for daily reboot).

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt upgrade
 git submodule update --init --recursive
 sudo apt install xdotool -y
+sudo ./bash/add_cronjob.sh
 sudo ./bash/setup_lcd.sh
 echo "installation completed. Raspberry will reboot now"
 sleep 1
